c00371d14fcc797f1fe1201b4bf9f5f1eccba5b3
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2020-07-04 18:05:21 +0200

    add clustering in silhouette calculation

--- event_clustering/clustering.py
+++ event_clustering/clustering.py
@@ -4,17 +4,13 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 from sklearn.cluster import MiniBatchKMeans
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 
 # from https://www.kaggle.com/jbencina/clustering-documents-with-tfidf-and-kmeans
 
 ##  interpret clusters
 # find_optimal_clusters(text, 20)
 # clusters = MiniBatchKMeans(n_clusters=6, init_size=1024, batch_size=2048, random_state=20).fit_predict(text)
-# plot_tsne_pca(text, clusters)
-# get_top_keywords(text, clusters, tfidf.get_feature_names(), 6)
 
 def find_optimal_clusters(data, max_k):
     iters = range(2, max_k+1, 2)
@@ -32,29 +28,9 @@ def find_optimal_clusters(data, max_k):
     ax.set_ylabel('SSE')
     ax.set_title('SSE by Cluster Center Plot')
 
-def plot_tsne_pca(data, labels):
-    max_label = max(labels)
-    max_items = np.random.choice(range(data.shape[0]), size=3000, replace=False)
-    
-    pca = PCA(n_components=2).fit_transform(data[max_items,:].todense())
-    tsne = TSNE().fit_transform(PCA(n_components=data.shape[1]).fit_transform(data[max_items,:].todense()))
-    
-    
-    idx = np.random.choice(range(pca.shape[0]), size=300, replace=False)
-    label_subset = labels[max_items]
-    label_subset = [cm.hsv(i/max_label) for i in label_subset[idx]]
-    
-    f, ax = plt.subplots(1, 2, figsize=(14, 6))
-    
-    ax[0].scatter(pca[idx, 0], pca[idx, 1], c=label_subset)
-    ax[0].set_title('PCA Cluster Plot')
-    
-    ax[1].scatter(tsne[idx, 0], tsne[idx, 1], c=label_subset)
-    ax[1].set_title('TSNE Cluster Plot')
-
-def get_top_keywords(data, clusters, labels, n_terms):
-    df = pd.DataFrame(data.todense()).groupby(clusters).mean()
-    
-    for i,r in df.iterrows():
-        print('\nCluster {}'.format(i))
-        print(','.join([labels[t] for t in np.argsort(r)[-n_terms:]]))
+def plot_pca(df, clusters):
+    pca = PCA(n_components=2).fit(df)
+    coords = pca.transform(df)
+    plt.figure()
+    plt.scatter(coords[:, 0], coords[:, 1], c=clusters, cmap='prism')
+    plt.show()
