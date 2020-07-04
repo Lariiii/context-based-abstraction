a5d9b90b2e56222edc6fefce8d0e81d1c1821164
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2020-07-04 14:04:46 +0200

    add one hot encoder

--- event_clustering/preprocessing.py
+++ event_clustering/preprocessing.py
@@ -1,18 +1,18 @@
 import os.path
 import datetime
 
-
-
 import pandas as pd
 
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.log.exporter.csv import factory as csv_exporter
 from pm4py.objects.log.importer.csv import factory as csv_importer
 
+from sklearn.preprocessing import OneHotEncoder
+
 TIMESTAMP_COLUMN_NAME = 'time:timestamp'
 
+# transform log into pandas dataframe, by writing it out as csv and reading it with pandas
 def load(file_path):
-    # transform log into pandas dataframe, by writing it out as csv and reading it with pandas
     csv_path = file_path + ".csv"
     if not os.path.isfile(csv_path):
         log = xes_importer.apply(file_path)
@@ -28,3 +28,8 @@ def drop_columns(df):
     for name in keep:
         column_names.remove(name)
     return df.drop(column_names, axis=1, inplace=False)
+
+def one_hot_encode(df, column, none_replacement):
+    enc = OneHotEncoder(handle_unknown='ignore')
+    df[column].fillna(none_replacement, inplace=True)
+    return pd.DataFrame(enc.fit_transform(df[[column]]).toarray(), columns = enc.get_feature_names([column]))
