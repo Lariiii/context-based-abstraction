b05cb58188018c7af078d21d331ba0b48752fb97
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2020-07-04 13:33:03 +0200

    transform timestamp to datetime object

--- event_clustering/preprocessing.py
+++ event_clustering/preprocessing.py
@@ -1,4 +1,7 @@
 import os.path
+import datetime
+
+
 
 import pandas as pd
 
@@ -6,13 +9,18 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.log.exporter.csv import factory as csv_exporter
 from pm4py.objects.log.importer.csv import factory as csv_importer
 
+TIMESTAMP_COLUMN_NAME = 'time:timestamp'
+
 def load(file_path):
     # transform log into pandas dataframe, by writing it out as csv and reading it with pandas
     csv_path = file_path + ".csv"
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(csv_path):
         log = xes_importer.apply(file_path)
         csv_exporter.export(log, csv_path)
-    return pd.read_csv(csv_path)
+    df = pd.read_csv(csv_path)
+    #  transform the timestamp str to datetime object
+    df[TIMESTAMP_COLUMN_NAME] = df[TIMESTAMP_COLUMN_NAME].apply(lambda x: datetime.datetime.fromisoformat(x))
+    return df
 
 def drop_columns(df):
     keep = ['concept:name']
